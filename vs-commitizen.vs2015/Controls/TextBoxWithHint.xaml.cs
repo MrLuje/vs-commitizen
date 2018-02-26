a46e4850728a2316acc2b2c5b59861e35c6ac8bd
MrLuje <ogmniluje@gmail.com>
2018-02-26 19:00:06 +0100

    fix: TextBoxWithHint binding

--- vs-commitizen.vs2015/Controls/TextBoxWithHint.xaml.cs
+++ vs-commitizen.vs2015/Controls/TextBoxWithHint.xaml.cs
@@ -1,19 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace vs_commitizen.vs2015.Controls
 {
@@ -48,13 +36,8 @@ namespace vs_commitizen.vs2015.Controls
 
         public string Text
         {
-            get => this.txtInputBox.Text;
-            set
-            {
-                this.txtInputBox.Text = value;
-                SetValue(TextProperty, value);
-                OnPropertyChanged();
-            }
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
         }
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(TextBoxWithHint));
 
