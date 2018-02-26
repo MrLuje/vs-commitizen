4f89c3a5b8162ef08704bc626b14fb22941786cf
MrLuje <ogmniluje@gmail.com>
2018-02-26 15:47:05 +0100

    feat(ui): Only enable navigation link on git

--- vs-commitizen.vs2015/VsCommitizenNavigationItem.cs
+++ vs-commitizen.vs2015/VsCommitizenNavigationItem.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.TeamFoundation.Controls;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.ComponentModel.Composition;
 using System.Drawing;
@@ -11,6 +12,9 @@ namespace vs_commitizen.vs2015
     public class VsCommitizenNavigationItem : TeamExplorerBaseNavigationItem
     {
         public const string NavigationItemId = "E3396357-5CFC-47A4-AECA-E52C894EBBDD";
+
+        private static readonly Guid TfsProviderGuid = new Guid("4CA58AB2-18FA-4F8D-95D4-32DDF27D184C");
+        private static readonly Guid GitProviderGuid = new Guid("11b8e6d7-c08b-4385-b321-321078cdd1f8");
 
         [ImportingConstructor]
         public VsCommitizenNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
@@ -25,6 +29,19 @@ namespace vs_commitizen.vs2015
         {
             var teamExplorer = GetService<ITeamExplorer>();
             teamExplorer?.NavigateToPage(Guid.Parse(VsCommitizenPage.PageId), null);
+        }
+
+        public override void Invalidate()
+        {
+            base.Invalidate();
+
+            var currentProviderGuid = Guid.Empty;
+#pragma warning disable VSTHRD010 // Use VS services from UI thread
+            var service = GetService<IVsRegisterScciProvider>() as IVsGetScciProviderInterface;
+            service?.GetSourceControlProviderID(out currentProviderGuid);
+#pragma warning restore VSTHRD010 // Use VS services from UI thread
+
+            this.IsVisible = currentProviderGuid == GitProviderGuid;
         }
     }
 }
