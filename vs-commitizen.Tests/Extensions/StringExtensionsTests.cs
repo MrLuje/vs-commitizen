7cd791d856065c4141115ba52e45a74a5fd9e9d3
MrLuje <ogmniluje@gmail.com>
2018-03-18 00:49:49 +0100

    feat: Split comment preserves words

---
+++ vs-commitizen.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vs_commitizen.Tests.TestAttributes;
+using vs_commitizen.vs.Extensions;
+using Xunit;
+
+namespace vs_commitizen.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        const string LP = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur risus nec erat varius, ornare accumsan diam ornare. Etiam ornare augue ut tempor vestibulum. Nullam libero metus, porta sollicitudin sapien sed, sollicitudin malesuada mauris. Curabitur hendrerit fermentum faucibus. Nullam turpis felis, interdum eu feugiat non, iaculis eget nisi. Praesent finibus volutpat nulla, quis accumsan metus congue eu. Praesent pulvinar auctor metus, in efficitur tortor congue a. Pellentesque sollicitudin mattis metus, quis mollis lectus tempor sed. Vivamus a gravida lacus. Vivamus volutpat ac erat nec rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;";
+
+        [Fact, TestConventions]
+        public void Split_At_Words_End()
+        {
+            var res = LP.ChunkBySizePreverveWords(50);
+            res.First().ShouldBe("Lorem ipsum dolor sit amet, consectetur adipiscing");
+        }
+
+        [Fact, TestConventions]
+        public void Remove_Leading_Space()
+        {
+            var res = LP.ChunkBySizePreverveWords(50);
+            res.First().ShouldBe("Lorem ipsum dolor sit amet, consectetur adipiscing");
+            res.Skip(1).First().ShouldStartWith("elit. Duis");
+        }
+
+        [Fact, TestConventions]
+        public void Split_At_LeadingSpace_If_Word_Too_Long()
+        {
+            var res = "Lorem ipsum dolor sit amet, consectetur adipiscingzomgitstoolong!".ChunkBySizePreverveWords(50);
+            res.ShouldBe(new[] { "Lorem ipsum dolor sit amet, consectetur", "adipiscingzomgitstoolong!" });
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Returns_Empty_For_Null_Empty(string str)
+        {
+            StringExtensions.ChunkBySizePreverveWords(str, 0).ShouldBeEmpty();
+        }
+
+        [Fact, TestConventions]
+        public void Split_By_1()
+        {
+            var res = "Lorem ipsum ".ChunkBySizePreverveWords(1);
+            res.ShouldBe(new[] { "L", "o", "r", "e", "m", "", "i", "p", "s", "u", "m", "" });
+        }
+    }
+}
--- vs-commitizen.vs2015/Extensions/StringExtensions.cs
+++ vs-commitizen.vs2015/Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,27 @@ namespace vs_commitizen.vs.Extensions
 {
     public static class StringExtensions
     {
-        public static IEnumerable<string> ChunkBySize(this string str, int chunkSize)
+        public static IEnumerable<string> ChunkBySizePreverveWords(this string str, int chunkSize)
         {
-            for (int i = 0; i < str.Length; i += chunkSize)
-                yield return str.Substring(i, Math.Min(chunkSize, str.Length - i));
+            if (string.IsNullOrWhiteSpace(str) || chunkSize == 0) return Enumerable.Empty<string>();
+            if (str.Length < chunkSize) return new[] { str.Trim() };
+
+            var currentIndex = 0;
+            var res = new List<string>();
+
+            while (currentIndex < str.Length)
+            {
+                var hasSpace = str.IndexOf(' ') >= 0;
+                int remainingStrLength = str.Length - currentIndex;
+                int remaningOrChunkSize = Math.Min(chunkSize, remainingStrLength);
+                var nextLine = hasSpace ?
+                               str.Substring(currentIndex, Math.Min(remaningOrChunkSize, str.LastIndexOf(' ') + 1)) :
+                               str.Substring(currentIndex, remaningOrChunkSize);
+
+                currentIndex += nextLine.Length;
+                res.Add(nextLine.Trim());
+            }
+            return res;
         }
 
         public static string SafeTrim(this string str)
