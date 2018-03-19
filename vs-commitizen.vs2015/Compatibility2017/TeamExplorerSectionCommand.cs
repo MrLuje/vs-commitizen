9bc4a1b55bd53a7efa3baff4b2b93923eefd2f8e
MrLuje <ogmniluje@gmail.com>
2018-03-19 21:26:44 +0100

    Add shortcut for settings in section

---
+++ vs-commitizen.vs2015/Compatibility2017/TeamExplorerSectionCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+using System.Windows.Media;
+
+namespace vs_commitizen.vs
+{
+    internal class TeamExplorerSectionCommand : ITeamExplorerSectionCommand
+    {
+        private ICommand command;
+        private string v;
+        private DrawingBrush drawingBrush;
+
+        public TeamExplorerSectionCommand(ICommand command, string v, DrawingBrush drawingBrush)
+        {
+            this.command = command;
+            this.v = v;
+            this.drawingBrush = drawingBrush;
+        }
+    }
+}
--- vs-commitizen.vs2015/VsCommitizenSection.cs
+++ vs-commitizen.vs2015/VsCommitizenSection.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.TeamFoundation.Controls;
+using Microsoft.TeamFoundation.Controls.WPF;
+using Microsoft.TeamFoundation.Controls.WPF.TeamExplorer;
+using Microsoft.TeamFoundation.MVVM;
 using System;
+using System.Windows.Input;
+using System.Windows.Media;
 using vs_commitizen.vs;
 using vs_commitizen.vs.Models;
 
 namespace vs_commitizen.vs2015
 {
     [TeamExplorerSection(VsCommitizenSection.SectionId, VsCommitizenPage.PageId, 30)]
-    public class VsCommitizenSection : TeamExplorerBaseSection
+    public class VsCommitizenSection : TeamExplorerBaseSection, ITeamExplorerSectionCommandProvider
     {
         private const string SectionId = "50948F33-9223-4E8C-A8A5-37A6225AE4E2";
+        private TeamExplorerSectionCommand teamExplorerSectionCommand;
 
         public VsCommitizenView CommitizenSection => this.SectionContent as VsCommitizenView;
+
+        public ITeamExplorerSectionCommand[] SectionCommands => new ITeamExplorerSectionCommand[] { teamExplorerSectionCommand };
 
         public VsCommitizenSection()
         {
@@ -19,6 +27,8 @@ namespace vs_commitizen.vs2015
             this.IsExpanded = true;
             this.IsBusy = false;
             this.SectionContent = new VsCommitizenView();
+
+            teamExplorerSectionCommand = new TeamExplorerSectionCommand((ICommand)null, "Options", WpfUtil.SharedResources["Ellipsis16IconBrush"] as DrawingBrush);
         }
 
         public override void Loaded(object sender, SectionLoadedEventArgs e)
