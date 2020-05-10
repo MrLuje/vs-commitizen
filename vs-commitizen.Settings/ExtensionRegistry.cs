cc9f222ce4aa12010b16cc42dd5ed5eda79e7579
MrLuje <ogmniluje@gmail.com>
2020-05-11 00:08:30 +0200

    Fix changes view (#31)
    * proper target version to mef components
    
    * fix: only load proper version of assemblies
    
    Previous version was loading all versions (2015, 2017, 2019) due to assembly scanning, now we only pick the proper one
    
    * fix: "open local config" not enabled if file doesn't exist

--- vs-commitizen.Settings/ExtensionRegistry.cs
+++ vs-commitizen.Settings/ExtensionRegistry.cs
@@ -14,7 +14,22 @@ namespace vs_commitizen.Settings
             this.Scan(i =>
             {
                 i.TheCallingAssembly();
-                i.AssembliesFromPath(Path.GetDirectoryName(Assembly.GetCallingAssembly().Location));
+                i.Assembly("vs-commitizen");
+
+                var productMajorPart = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileVersionInfo.ProductMajorPart;
+                if (productMajorPart == 14)
+                {
+                    i.Assembly("vs-commitizen.vs2015");
+                }
+                else if (productMajorPart == 15)
+                {
+                    i.Assembly("vs-commitizen.vs2017");
+                }
+                else if (productMajorPart >= 16)
+                {
+                    i.Assembly("vs-commitizen.vs2019");
+                }
+
                 i.WithDefaultConventions();
                 i.LookForRegistries();
             });
