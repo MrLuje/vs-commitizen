b5f1fbc3a7c973e577009fcea9af41d5bd2bf8a0
MrLuje <ogmniluje@gmail.com>
2021-05-09 16:40:41 +0200

    add icon to tool window

--- vs-commitizen/VsCommitizenIds.cs
+++ vs-commitizen/VsCommitizenIds.cs
@@ -32,6 +32,9 @@ namespace vs_commitizen
 
         public const string guidImages2String = "b8ba0b3c-2fca-4a93-9fda-70276a399b8f";
         public static Guid guidImages2 = new Guid(guidImages2String);
+
+        public const string guidImages3String = "8851452f-ce33-4455-9345-d17c80586bdb";
+        public static Guid guidImages3 = new Guid(guidImages3String);
     }
     /// <summary>
     /// Helper class that encapsulates all CommandIDs uses across VS Package.
@@ -50,5 +53,6 @@ namespace vs_commitizen
         public const int bmpPicArrows = 0x0005;
         public const int bmpPicStrikethrough = 0x0006;
         public const int bmpOpen = 0x0001;
+        public const int gitIcon = 0x0001;
     }
 }
