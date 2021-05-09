232083d6249b282447460d5ae31aa6ba26a88925
MrLuje <ogmniluje@gmail.com>
2021-05-09 20:09:25 +0200

    use style instead of MarginConverter

--- vs-commitizen.vs2015/Converters/MarginConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace vs_commitizen.vs.Converters
-{
-    public class MarginConverter : BooleanConverter<Thickness>
-    {
-        public MarginConverter() : base(new Thickness(0, 0, 0, 0), new Thickness(10, 5, 0, 0)) { }
-    }
-}
