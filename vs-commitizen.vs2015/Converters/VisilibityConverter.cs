502739bcc60a320abd0e7d322b9eeed2a20e7ba7
MrLuje <ogmniluje@gmail.com>
2021-11-29 20:52:21 +0100

    cleanup old vs2015

--- vs-commitizen.vs2015/Converters/VisilibityConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace vs_commitizen.vs.Converters
-{
-    public class VisilibityConverter : BooleanConverter<Visibility>
-    {
-        public VisilibityConverter() : base(Visibility.Visible, Visibility.Hidden) { }
-    }
-}
