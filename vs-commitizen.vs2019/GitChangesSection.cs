b96f17d6f0e61484e07c5ed9006e52710484e15c
MrLuje <ogmniluje@gmail.com>
2021-12-11 14:35:47 +0100

    fix: add vs2022 lib & proper version split

---
+++ vs-commitizen.vs2019/GitChangesSection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.TeamFoundation.Controls.WPF.TeamExplorer;
+using System.ComponentModel;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using vs_commitizen.vs;
+using vs_commitizen.vs.Models;
+
+namespace vs_commitizen.vs2015
+{
+    public partial class GitChangeSection : TeamExplorerBaseSection
+    {
+        private void TeamExplorerPageBasePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var page = (TeamExplorerPageBase)sender;
+
+            // Wait for section to be not busy
+            if (page.IsBusy)
+                return;
+
+            var commitData = PopNavigationValue<NavigationCommitModel>(NavigationDataType.CommitData);
+            if (commitData == null) return;
+
+            var model = page.Model;
+            var commentProperty = model.GetType().GetProperty("Comment");
+
+            commentProperty.SetValue(model, commitData.Comment);
+
+            if (!commitData.AutoCommit) return;
+
+            var changesExt = GetService<Microsoft.TeamFoundation.Git.Controls.Extensibility.IChangesExt2>();
+            var hasPendingChanges = changesExt.IncludedChanges.Count > 0 || changesExt.UntrackedFiles.Count > 0;
+
+            if (!hasPendingChanges) return;
+
+            var peer = new ButtonAutomationPeer(commitButton);
+            var invokeProv = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+            invokeProv.Invoke();
+        }
+    }
+}
---
+++ vs-commitizen.vs2022/GitChangesSection.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using vs_commitizen.vs;
+
+namespace vs_commitizen.vs2015
+{
+    public partial class GitChangeSection : TeamExplorerBaseSection
+    {
+        private void TeamExplorerPageBasePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+        }
+    }
+}
